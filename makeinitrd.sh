7551ed3659f0f882b875ef42da51a4070d6146df
teddyxiong53 <1073167306@qq.com>
2018-03-16 16:14:19 +0800

    改为从kernel直接启动的方式。

--- makeinitrd.sh
+++ makeinitrd.sh
@@ -1,17 +1,18 @@
 #!/bin/sh 
 
-set -e
+#set -e
 
 ROOR_DIR=`pwd`
 
 
-rm -f $ROOR_DIR/ramdisk.img.gz
+rm -f $ROOR_DIR/ramdisk.img
 rm -f $ROOR_DIR/ramdisk.img.uboot
 
+umount -f /mnt/initrd
 
 RDSIZE=4096
 BLKSIZE=1024
-BUSYBOX=/home/teddy/work/mylinuxlab/rootfs/bin/busybox
+ROOTFS=/home/teddy/work/mylinuxlab/rootfs
 
 dd if=/dev/zero of=$ROOR_DIR/ramdisk.img bs=${BLKSIZE} count=$RDSIZE
 
@@ -19,50 +20,9 @@ mke2fs -F -m 0 -b $BLKSIZE $ROOR_DIR/ramdisk.img $RDSIZE
 
 mount $ROOR_DIR/ramdisk.img /mnt/initrd -t ext2 -o loop
 
-mkdir /mnt/initrd/bin 
-mkdir /mnt/initrd/sys 
-mkdir /mnt/initrd/dev 
-mkdir /mnt/initrd/proc
+cd /mnt/initrd;cp -rf $ROOTFS/* ./; chmod 666 ./dev/ram0; chmod 777 ./bin/*; chmod 777 ./init; cd -
 
-cd /mnt/initrd/bin
-
-cp $BUSYBOX ./
-ln -s busybox ash
-ln -s busybox mount
-ln -s busybox echo
-ln -s busybox ls
-ln -s busybox cat
-ln -s busybox ps
-cd -
-
-cp -a /dev/console /mnt/initrd/dev
-#cp -a /dev/ramdisk /mnt/initrd/dev
-cp -a /dev/null /mnt/initrd/dev
-mknod -m 660 /mnt/initrd/dev/ram0 b 1 1
-ln -s /mnt/initrd/dev/ram0 /mnt/initrd/dev/ramdisk
-
-cp -a /dev/tty1 /mnt/initrd/dev
-cp -a /dev/tty2 /mnt/initrd/dev
-
-
-cd  /mnt/initrd
-ln -s bin sbin
-cd -
-
-# Create the init file
-cat >> /mnt/initrd/linuxrc << EOF
-#!/bin/ash
-echo
-echo "Simple initrd is active"
-echo
-mount -t proc /proc /proc
-mount -t sysfs none /sys
-/bin/ash --login
-EOF
-
-chmod +x /mnt/initrd/linuxrc
-
-
+read tmp
 
 cd /mnt/initrd
 find . | cpio -o -H newc | gzip -c > $ROOR_DIR/ramdisk.img
@@ -70,5 +30,5 @@ cd -
 # Finish up...
 umount /mnt/initrd
 
-gzip -9 $ROOR_DIR/ramdisk.img
+#gzip -9 $ROOR_DIR/ramdisk.img
 
